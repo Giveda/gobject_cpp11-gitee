33cb9507471e14f85c78b80fa83966068081e17d
Giveda <692463260@qq.com>
2019-08-15 21:02:57 +0800

    update license

--- gObject.h
+++ gObject.h
@@ -6,13 +6,10 @@
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.
- * This program is not a free software; so you can not redistribute it and/or
- * modify it without my authorization. If you only need use it for personal
- * study purpose(no redistribution, and without any  commercial behavior),
- * you should accept and follow the GNU AGPL v3 license, otherwise there
- * will be your's credit and legal risks.  And if you need use it for any
- * commercial purpose, you should first get commercial authorization from
- * me, otherwise there will be your's credit and legal risks.
+ * This program is not a free software; so you can not redistribute it(include
+ * binary form and source code form) without my authorization. And if you
+ * need use it for any commercial purpose, you should first get commercial
+ * authorization from me, otherwise there will be your's legal&credit risks.
  *
  */
 
